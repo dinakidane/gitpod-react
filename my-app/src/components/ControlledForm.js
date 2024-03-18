49ef9942f4ce1972421aad40159daca04ebbe438
dinakidane <dinakidane@hotmail.com>
2024-03-18 17:15:29 +0000

    more forms

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -9,17 +9,41 @@ export class ControlledForm extends Component {
       }
     }
 
+    handleNameChange = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
     render() {
         return (
         <div>
+            <h2>Please fill out the form:</h2>
             <form>
-                <label htmlFor='id-name'>Your Name:</label>
-                <input 
-                    value = {this.state.name} 
-                    id='id-name' 
-                    name='name' 
-                    type='text' />
-                <input type='submit' value='Submit' />
+                <div>
+                    <label htmlFor='id-name'>Your Name:</label>
+                    <input 
+                        value = {this.state.name} 
+                        onChange={this.handleNameChange}
+                        id='id-name' 
+                        name='name' 
+                        type='text' />
+                </div>
+
+                <div>
+                    <label htmlFor = 'id-category'>Query Category:</label>
+                    <select id='id-category' name='category'>
+                        <option value='website'>Website Issue</option> 
+                        <option value='order'>Order Issue</option>
+                        <option value='general'>General Inquiry</option>                   
+                    </select>
+                    <input type='submit' value='Submit' />
+                </div>
+
+                <div>
+                <label htmlFor = 'id-comments'>Comments:</label>
+                <textarea id='id-comments' name='comments' />
+                </div>
             </form>
         </div>
         )
